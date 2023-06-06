fcca1146a7702778099be6f4e7fba9a022d661b5
andwnqodn <mkasld@gotmail.com>
2023-06-05 21:58:46 -0500

    Agregar archivos de proyecto.

---
+++ ApiDemo/Controllers/DemoController.cs
@@ -0,0 +1,24 @@
+﻿using ApiDemo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+
+        [HttpGet(Name = "GetDemos")]
+        public IEnumerable<DemoModel> Get()
+        {
+            List<DemoModel> demos = new List<DemoModel>();
+            demos.Add(new DemoModel { Id= 1, Name= "Test"});
+            demos.Add(new DemoModel { Id= 2, Name= "Test2"});
+            demos.Add(new DemoModel { Id= 3, Name= "Test3"});
+            demos.Add(new DemoModel { Id= 4, Name= "Test4"});
+
+            return demos;
+        }
+    }
+}
---
+++ ApiDemo/Models/DemoContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace ApiDemo.Models
+{
+    public class DemoContext : DbContext
+    {
+        public DemoContext(DbContextOptions<DemoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Person> People { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
